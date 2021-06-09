96142368873ee4a78f15ac7a337acff7e973e344
Tigran <Tigranshukuryan2004@gmail.com>
2021-06-10 01:22:20 +0400

    n2

--- github_test.py
+++ github_test.py
@@ -1,1 +1,12 @@
 print("jghgv")
+class MagicFunction:
+    def __init__(self):
+        self.sum = 0
+    def __call__(self, *args):
+        for i in args:
+            self.sum += i
+        return self.sum
+
+e = MagicFunction()
+print(e(20, 30))
+
